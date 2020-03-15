a1b7b2f7b6297371913f66fcedb25f56b10a8099
Naitik Shah <n@daaku.org>
2020-03-15 12:02:45 +0400

    remove some choco installed applications

--- marzipan.cmd
+++ marzipan.cmd
@@ -1,14 +1,9 @@
 @powershell.exe -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 choco install -y 7zip
-choco install -y cyberduck
 choco install -y dropbox
 choco install -y firefox
 choco install -y foxitreader
-choco install -y googlechrome
 choco install -y irfanview
 choco install -y qBittorrent
-choco install -y skype
-choco install -y vcxsrv
 choco install -y vlc
-choco install -y whatsapp
